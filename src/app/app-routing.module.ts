b8e87460e0a19789e03fa7e5e2c03f2109e4f1db
rihabcherni <rihabcherni000@gmail.com>
2023-11-26 12:02:00 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent as AdminHomeComponent } from './admin/home/home.component';
+import { HomeComponent as VisiteurHomeComponent } from './visiteur/home/home.component';
+import {  HomeComponent as ResposableHomeComponent  } from './responsable-ecole/home/home.component';
+import {  HomeComponent as LivreurHomeComponent  } from './livreur/home/home.component';
+import {  HomeComponent as DonateurHomeComponent  } from './donateur/home/home.component';
+const routes: Routes = [
+  { path: '', component: VisiteurHomeComponent },
+  { path: 'admin', component: AdminHomeComponent },
+  { path: 'responsable-ecole', component: ResposableHomeComponent },
+  { path: 'livreur', component: LivreurHomeComponent },
+  { path: 'donateur', component: DonateurHomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
