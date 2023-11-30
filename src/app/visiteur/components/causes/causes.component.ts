76b54821c95bbca25ed8fe5d4b671d1570204b11
rihabcherni <rihabcherni000@gmail.com>
2023-11-30 20:37:54 +0100

    30/11

--- src/app/visiteur/components/causes/causes.component.ts
+++ src/app/visiteur/components/causes/causes.component.ts
@@ -6,5 +6,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./causes.component.css']
 })
 export class CausesComponent {
-
+  causes = [
+    {
+      id: "1",
+      school:"École primaire Habib Bourguiba à Kairouan",
+      img: "assets/img/visiteur/s1.jfif",
+      moreInfo: "More Info",
+      adresse:"Rue El Hanchi, Kairouan 8100, Tunisie",
+      besoin:["Matériel scolaire", "fournitures informatiques", "rénovation des locaux"],
+      class:"20",
+      teacher:"25",
+      student:"500",
+      ambassador:"Jhon Doe",
+      ambassadorRole:"Teacher"
+    },
+    {
+      id: "2",
+      school:"École primaire Ibn Khaldoun à Sfax",
+      img: "assets/img/testimonial-3.jpg",
+      moreInfo: "More Info",
+      adresse:"",
+      besoin:"",
+    },
+    {
+      id: "3",
+      school:"school Name",
+      img: "assets/img/testimonial-3.jpg",
+      moreInfo: "More Info",
+      adresse:"",
+      besoin:"",
+    },
+    {
+      id: "4",
+      school:"school Name",
+      img: "assets/img/testimonial-3.jpg",
+      moreInfo: "More Info",
+      adresse:"",
+      besoin:"",
+    },
+    {
+      id: "5",
+      school:"school Name",
+      img: "assets/img/testimonial-3.jpg",
+      moreInfo: "More Info",
+      adresse:"",
+      besoin:"",
+    },
+    {
+      id: "6",
+      school:"school Name",
+      img: "assets/img/testimonial-3.jpg",
+      moreInfo: "More Info",
+      adresse:"",
+      besoin:"",
+    },
+  ];
 }
