47faf483c80b91ea0faac886d7819da9cbac1472
rihabcherni <rihabcherni000@gmail.com>
2023-12-24 18:27:32 +0100

    24/12

--- src/app/responsable-ecole/components/gestion-liste-besoins-ecole/gestion-liste-besoins-ecole.component.ts
+++ src/app/responsable-ecole/components/gestion-liste-besoins-ecole/gestion-liste-besoins-ecole.component.ts
@@ -10,36 +10,49 @@ import { GestionSchoolService } from 'src/app/services/SchoolManagement/gestion-
 })
 export class GestionListeBesoinsEcoleComponent {
   constructor(private schoolService: GestionSchoolService,private authService: AuthServicesService, private datePipe: DatePipe) { }
-  selectedItemsSchool: any[] = [
-    {typeNeeds: "pen", quantity: 10},
-    {typeNeeds: "book", quantity: 15},
-  ];
-  selectedItemsClothes: any[] = [
-    {typeNeeds: "pen", quantity: 10},
-    {typeNeeds: "book", quantity: 15},
-  ];
-  selectedItemsMoney: any[] = [
-    {quantity: 10},
-    { quantity: 15},
-  ];
+  school: any={name:"", address:"",governorate:"",niveau:"",nbr_student:"",nbr_teachers:"",nbr_classes:"", needs:"", type_needs:"", DetailsNeeds:""};
+  getSchoolDetails(): void {
+    const id = this.authService.getSchoolId() || "";
+    this.schoolService.getSchoolDetails(id).subscribe(
+      (data: any) => {
+        console.log("Response from API:", data);
+        this.school = data;
+      },
+      (error: any) => {
+        console.error('Error fetching schools:', error);
+      }
+    );
+  }
+  newSchool: any;
+  ngOnInit(): void {
+    this.getSchoolDetails();
+    this.newSchool = {
+      _id: this.authService.getSchoolId() || "",
+      DetailsNeeds: this.school.DetailsNeeds
+    };
+  }
+  updateSchool(): void {
+    const id = this.authService.getSchoolId() || "";
+    this.schoolService.updateSchool(id, this.newSchool).subscribe(
+      (response: any) => {
+        console.log('School mis à jour avec succès :', response);
+        this.authService.setUser(this.newSchool);
+        alert("Your school update successfuly");
+      },
+      (error: any) => {
+        console.error('Erreur lors de la mise à jour du school :', error);
+
+      }
+    );
+  }
+
   typeNeeds: string = '';
   quantity: string = '';
-  addSchool(type: string, quantity: string): void {
+
+  addItemsSchool(type: string, quantity: string): void {
     this.typeNeeds = type;
     this.quantity = quantity;
-    this.selectedItemsSchool.push({ typeNeeds: this.typeNeeds, quantity: this.quantity });
-    this.openDetailsModal();
-  }
-  addCloths(type: string, quantity: string): void {
-    this.typeNeeds = type;
-    this.quantity = quantity;
-    this.selectedItemsClothes.push({ typeNeeds: this.typeNeeds, quantity: this.quantity });
-    this.openDetailsModal();
-  }
-  addMoney(type: string, quantity: string): void {
-    this.typeNeeds = type;
-    this.quantity = quantity;
-    this.selectedItemsMoney.push({ uantity: this.quantity });
+    this.school.DetailsNeeds.items.push({ type: this.typeNeeds, nimber: this.quantity });
     this.openDetailsModal();
   }
   clearInputFields(): void {
@@ -58,41 +71,5 @@ export class GestionListeBesoinsEcoleComponent {
       modal.style.display = 'none';
     }
     this.clearInputFields();
-  }
-
-  school: any={name:"", address:"",governorate:"",niveau:"",nbr_student:"",nbr_teachers:"",nbr_classes:"", needs:"", type_needs:""};
-  getSchoolDetails(): void {
-    const id = this.authService.getSchoolId() || "";
-    this.schoolService.getSchoolDetails(id).subscribe(
-      (data: any) => {
-        console.log("Response from API:", data);
-        this.school = data;
-      },
-      (error: any) => {
-        console.error('Error fetching schools:', error);
-      }
-    );
-  }
-  newSchool: any;
-  ngOnInit(): void {
-    this.getSchoolDetails();
-    this.newSchool = {
-      _id: this.authService.getSchoolId() || "",
-      name: this.school.name,
-    };
-  }
-  updateSchool(): void {
-    const id = this.authService.getSchoolId() || "";
-    this.schoolService.updateSchool(id, this.newSchool).subscribe(
-      (response: any) => {
-        console.log('School mis à jour avec succès :', response);
-        this.authService.setUser(this.newSchool);
-        alert("Your school update successfuly");
-      },
-      (error: any) => {
-        console.error('Erreur lors de la mise à jour du school :', error);
-
-      }
-    );
   }
 }
